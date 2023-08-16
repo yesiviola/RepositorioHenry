1ca0ed5740e7c6cb21d42019946ca65d364b2b79
“yesenia <aryesigon@gmail.com>
2023-08-16 09:24:45 -0300

    tarea realizada del prepcourse henry M07 javascript clase

--- M07 JavaScript Clases/Ejercicio 02.js
+++ M07 JavaScript Clases/Ejercicio 02.js
@@ -6,21 +6,63 @@ class Persona {
    // Debe tener las propiedades: "nombre", "apellido", "edad" y "domicilio".
    // Debe tener un método llamado "detalle" que nos devuelva un objeto con las propiedades de la persona y
    // sus valores.
-}
+   constructor(nombre, apellido, edad, domicilio) {
+      this.nombre = nombre;
+      this.apellido = apellido;
+      this.edad = edad;
+      this.domicilio = domicilio;
+    }
+  
+    // Método "detalle" que devuelve un objeto con las propiedades de la persona y sus valores.
+    detalle() {
+      return {
+        nombre: this.nombre,
+        apellido: this.apellido,
+        edad: this.edad,
+        domicilio: this.domicilio
+      };
+    }
+  }
+  // Crear una instancia de Persona
+  const persona1 = new Persona("Juan", "Pérez", 30, "Calle 123");
+  
+  // Obtener los detalles de la persona utilizando el método detalle
+  const detallesPersona1 = persona1.detalle();
+  console.log(detallesPersona1);
+  
+    
+    
+   function crearInstanciaPersona(nombre, apellido, edad, domicilio) {
+      // En este ejercicio debes crear una instancia de la clase construida en el ejercicio anterior.
+      // Recibirás las propiedades por parámetro.
+      // Retornar la instancia creada.
+      // Tu código:
+      const nuevaPersona = new Persona(nombre, apellido, edad, domicilio);
+      return nuevaPersona;
+    }
+       // Llamamos a la función para crear una instancia de Persona
+const persona2 = crearInstanciaPersona("Ana", "García", 25, "Avenida 456");
+console.log(persona2.detalle()); // Imprimirá los detalles de la persona
 
-function crearInstanciaPersona(nombre, apellido, edad, domicilio) {
-   // En este ejercicio debes crear una instancia de la clase construida en el ejercicio anterior.
-   // Recibirás las propiedades por parámetro.
-   // Retornar la instancia creada.
-   // Tu código:
-}
+const persona3 = crearInstanciaPersona("Carlos", "Martínez", 35, "Calle 789");
+console.log(persona3.detalle()); // Imprimirá los detalles de la persona
 
-function agregarMetodo() {
-   // La función agrega un método "datos" a la clase "Persona".
-   // Este método toma la propiedad "nombre" y "edad", y devuelve el string:
-   // Ejemplo: "Juan, 22 años".
-   // Tu código:
-}
+
+   function agregarMetodo() {
+      // La función agrega un método "datos" a la clase "Persona".
+      // Este método toma la propiedad "nombre" y "edad", y devuelve el string:
+      // Ejemplo: "Juan, 22 años".
+      // Tu código:
+      Persona.prototype.datos = function() {
+        return `${this.nombre}, ${this.edad} años`;
+      };
+    }
+agregarMetodo();
+
+const persona = new Persona("María", "López", 28, "Calle 234");
+
+console.log(persona.datos()); 
+  
 
 /*⚠️ NO MODIFIQUES NADA DEBAJO DE ESTO ⚠️*/
 module.exports = {
